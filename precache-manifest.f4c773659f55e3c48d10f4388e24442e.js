71416a3d1e051d63ee44964ec63ced7c38a29a6d
bokor-git <bogdankorolbk@gmail.com>
2020-05-03 13:18:00 +0300

    Updates

---
+++ precache-manifest.f4c773659f55e3c48d10f4388e24442e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f164b9c0cd7fce74261dfbbbea79851b",
+    "url": "/git@github.com:bokor-git/incubator.git/index.html"
+  },
+  {
+    "revision": "c6e60b845b26d9f5974e",
+    "url": "/git@github.com:bokor-git/incubator.git/static/css/main.752745cc.chunk.css"
+  },
+  {
+    "revision": "525dd7346099cb8d7a0f",
+    "url": "/git@github.com:bokor-git/incubator.git/static/js/2.d86acbd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:bokor-git/incubator.git/static/js/2.d86acbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e60b845b26d9f5974e",
+    "url": "/git@github.com:bokor-git/incubator.git/static/js/main.94bd2c0a.chunk.js"
+  },
+  {
+    "revision": "6e5e53fd83cdcceb74cc",
+    "url": "/git@github.com:bokor-git/incubator.git/static/js/runtime-main.c5da8bca.js"
+  }
+]);
