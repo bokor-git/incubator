dac7a5aef16f7500890f6ef49ff4af80df5497d7
bokor-git <bogdankorolbk@gmail.com>
2020-05-09 14:22:23 +0300

    done Loops task

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.css';
 import TodoList from "./Todolist/Todolist";
 import {sum} from "./lessons/lessons1"
 import {imp} from "./lessons/lessons2"
+import {imp1} from "./Tasks/Loops";
 
-sum(12, imp)
+sum(imp1, imp)
 
 function App() {
   return (
